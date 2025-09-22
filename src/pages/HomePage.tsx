cedef3bdaa4e01080a6426f25717e36d7e8e3dc5
Dawid Pionk <dawidpionk01@gmail.com>
2025-09-22 16:56:00 +0100

    Removed vite components and added postcss

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return (
+        <main>
+            <h1>Welcome to Louisa's Website</h1>
+            <p>This is the home page.</p>
+        </main>
+    );
+};
+
+export default HomePage;
