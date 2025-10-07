3975d6a8406a53afdb9994daf09f98472662c27a
Dawid Pionk <150350371+DawidP2001@users.noreply.github.com>
2025-10-08 00:09:25 +0100

    Completed the instagram and featured pattern sections

--- src/components/Nav-Button.tsx
+++ src/components/Nav-Button.tsx
@@ -15,7 +15,7 @@ const NavButton: React.FC<NavButtonProps> = ({
   isActive = false,
   className = '' 
 }) => {
-  const baseStyles = 'px-4 py-1 border border-black max-w-[200px] shadow-lg transform transition-all duration-150 active:scale-95';
+  const baseStyles = 'px-4 py-1 border rounded-3xl border-black max-w-[200px] shadow-lg transform transition-all duration-150 active:scale-95';
   const activeStyles = isActive 
     ? 'bg-gradient-to-b from-[#FF8080] to-[#FF5C5C] text-white shadow-inner' 
     : 'bg-gradient-to-b from-[#FFD0D0] to-[#FFAFAF] text-black hover:from-[#FF8080] hover:to-[#FF5C5C] hover:shadow-xl';
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,30 +12,61 @@ const HomePage: React.FC<HomePageProps> = ({ setActiveButton }) => {
         <div className='grid grid-cols-3 gap-1 mt-4'>
             <div className='p-1 rounded shadow'>
                 <NavigationCard setActiveButton={setActiveButton} />
-                <Card title="Instagram">
-                    <p className="text-center text-black">Follow me on Instagram for the latest updates and behind-the-scenes looks at my crafting projects!</p>
-                </Card>
+                <a href='https://www.instagram.com/caillougarage/' target="_blank" rel="noopener noreferrer">
+                    <Card>
+                        <div className='text-center flex flex-row items-center justify-center'>
+                            <img src='icons/instagram.png' alt="Instagram Icon" className="mx-auto mb-2 w-12" />
+                            <h3 className="text-lg text-center mb-4 press-start-2p-regular" style={{ color: 'black' }}>
+                                Instagram
+                            </h3>
+                        </div>
+                        <div className="slider">
+                            <div className="slide-track">
+                                <img src="/slideshow/1.PNG" alt="Instagram 1" className="mx-auto mb-2" />
+                                <img src="/slideshow/2.PNG" alt="Instagram 2" className="mx-auto mb-2" />
+                                <img src="/slideshow/3.PNG" alt="Instagram 3" className="mx-auto mb-2" />
+                                <img src="/slideshow/4.PNG" alt="Instagram 4" className="mx-auto mb-2" />
+                                <img src="/slideshow/5.PNG" alt="Instagram 5" className="mx-auto mb-2" />
+                                <img src="/slideshow/6.PNG" alt="Instagram 6" className="mx-auto mb-2" />
+                                <img src="/slideshow/7.PNG" alt="Instagram 7" className="mx-auto mb-2" />
+                                <img src="/slideshow/8.PNG" alt="Instagram 8" className="mx-auto mb-2" />
+                                <img src="/slideshow/1.PNG" alt="Instagram 1" />
+                                <img src="/slideshow/2.PNG" alt="Instagram 2" />
+                                <img src="/slideshow/3.PNG" alt="Instagram 3" />
+                                <img src="/slideshow/4.PNG" alt="Instagram 4" />
+                                <img src="/slideshow/5.PNG" alt="Instagram 5" />
+                                <img src="/slideshow/6.PNG" alt="Instagram 6" />
+                                <img src="/slideshow/7.PNG" alt="Instagram 7" />
+                                <img src="/slideshow/8.PNG" alt="Instagram 8" />
+                            </div>
+                        </div>
+                        <p className="text-justify text-black">Follow me on Instagram for the latest updates and behind-the-scenes looks at my crafting projects!</p>
+                    </Card>
+                </a>
             </div>
             <div className='p-1 rounded shadow'>
                 <Card title="Welcome">
                     <div className="wave text-center mx-auto"></div>
-                    <p className="text-center text-black">
-                        Hello! I'm Louisa, and welcome to my crafting corner of the internet. Here you'll find a collection of my favorite knit and crochet patterns, 
-                        tips, and projects. Whether you're a seasoned crafter or just starting out, I hope you find something that inspires you!
+                    <p className="text-justify text-black">
+                        Hello! I'm Louisa, and welcome to my caillougarage website. Here you'll find a collection of my favorite knit and crochet patterns, 
+                        tips, and projects. I hope you enjoy exploring and find inspiration for your own crafting journey!
                         </p>
                 </Card>
                 <Card title="From the Blog">
-                    <p className="text-left text-black">New Blog Post: "5 Tips for Perfecting Your Knit Stitches" - Check out my latest blog post where I share my top tips for achieving flawless knit stitches every time. Whether you're a beginner or looking to refine your skills, these tips will help you take your knitting to the next level!</p>
+                    <p className="text-justify text-black">New Blog Post: "5 Tips for Perfecting Your Knit Stitches" - Check out my latest blog post where I share my top tips for achieving flawless knit stitches every time. Whether you're a beginner or looking to refine your skills, these tips will help you take your knitting to the next level!</p>
                 </Card>
             </div>
             <div className='p-1 rounded shadow'>
                 <Card title="Featured Pattern">
-                    <p className="text-center text-black">Check out my latest knit pattern, the Cozy Cable Sweater! Perfect for chilly days, this sweater combines classic cables with a modern fit. Click below to view the pattern and start knitting!</p>
+                    <p className='text-center text-black mb-5 text-lg' style={{marginTop: '-1rem'}}>Wales Crochet Pattern</p>
+                    <img src="/gifs/Wales Crochet GIF by Twin_Made.gif" alt="Wales Crochet Pattern" className="mx-auto mb-2" />
+                    <p className="text-justify text-black">Check out my latest knit pattern, the Cozy Cable Sweater! Perfect for chilly days, this sweater combines classic cables with a modern fit. Click below to view the pattern and start knitting!</p>
                 </Card>
                 <Card title="Contact me">
                     <img src="/gifs/Phone.gif" alt="Phone" className="mx-auto mb-2" />
-                    <p className="text-center text-black">Have questions or want to collaborate? Feel free to reach out to me via email at louisa@example.com</p>
+                    <p className="text-justify text-black">Have questions or want to collaborate? Feel free to reach out to me via email at louisa@example.com</p>
                 </Card>
+                <a href="https://www.flaticon.com/free-icons/instagram-logo" title="instagram logo icons">Instagram logo icons created by Pixel perfect - Flaticon</a>
             </div>
         </div>
     );
