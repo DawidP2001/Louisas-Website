e0428b7370dea0dcc0f2b395347339d1ccf2af8e
Dawid Pionk <150350371+DawidP2001@users.noreply.github.com>
2025-10-07 17:01:17 +0100

    Created an animated image for the welcome card

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Card from '../components/Card';
 import NavigationCard from '../components/NavigationCard';
-import Title from '../components/Title';
 
 interface HomePageProps {
   setActiveButton: (button: string) => void;
@@ -19,11 +18,14 @@ const HomePage: React.FC<HomePageProps> = ({ setActiveButton }) => {
             </div>
             <div className='p-1 rounded shadow'>
                 <Card title="Welcome">
-                    <img src="Idle.png" alt="Idle" className="mx-auto mb-2" />
-                    <p className="text-center text-black">Hello! I'm Louisa, and welcome to my crafting corner of the internet. Here you'll find a collection of my favorite knit and crochet patterns, tips, and projects. Whether you're a seasoned crafter or just starting out, I hope you find something that inspires you!</p>
+                    <div className="wave text-center mx-auto"></div>
+                    <p className="text-center text-black">
+                        Hello! I'm Louisa, and welcome to my crafting corner of the internet. Here you'll find a collection of my favorite knit and crochet patterns, 
+                        tips, and projects. Whether you're a seasoned crafter or just starting out, I hope you find something that inspires you!
+                        </p>
                 </Card>
                 <Card title="From the Blog">
-                    <p className="text-center text-black">New Blog Post: "5 Tips for Perfecting Your Knit Stitches" - Check out my latest blog post where I share my top tips for achieving flawless knit stitches every time. Whether you're a beginner or looking to refine your skills, these tips will help you take your knitting to the next level!</p>
+                    <p className="text-left text-black">New Blog Post: "5 Tips for Perfecting Your Knit Stitches" - Check out my latest blog post where I share my top tips for achieving flawless knit stitches every time. Whether you're a beginner or looking to refine your skills, these tips will help you take your knitting to the next level!</p>
                 </Card>
             </div>
             <div className='p-1 rounded shadow'>
